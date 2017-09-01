38c8b2e5021d5f17c17cd9141b8ed95fddee1574
SHIV1003 <goyalshivam1003@gmail.com>
2017-09-01 14:52:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.get('/create-user/:username/:password',function(req,res)
 });
 app.get('/check',function(req,res)
 {
+    var success = 'success';
      pool.query('SELECT * FROM "user" ',function (err,result)
     {
         if(err)
@@ -62,7 +63,7 @@ app.get('/check',function(req,res)
         else
         {
             console.log('successful');
-            return successful;
+            return success;
         }
     })
 });
