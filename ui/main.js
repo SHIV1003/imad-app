bd8555f3ba146c9fd943f5fece6696d30f8fe101
SHIV1003 <goyalshivam1003@gmail.com>
2017-09-01 16:46:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,18 @@
 var submit = document.getElementById('submit_bttn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
+      request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+              } 
+          }
+      };
     var username = document.getElementById('username').value;
     console.log(username);
     request.open('post','http://http://goyalshivam1003.imad.hasura-app.io/check1',true);
