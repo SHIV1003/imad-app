4a03a5c50970c31393782ccd30807321db123d98
SHIV1003 <goyalshivam1003@gmail.com>
2017-09-01 11:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/create-user/:username/:passwowrd',function(req,res)
 {
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function (err,result,)
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function (err,result)
     {
         if(err)
         {
