a66ac1631c00f2ceea42383abfd7907741bb6635
SHIV1003 <goyalshivam1003@gmail.com>
2017-09-01 16:28:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var submit = document.getElementById('submit_bttn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
-    var username = document.getElememntById('username').value;
+    var username = document.getElementById('username').value;
     console.log(username);
     request.open('post','http://http://goyalshivam1003.imad.hasura-app.io/check1',true);
-    request.sendRequestHeader('content-type','application/json');
+    request.setRequestHeader('content-type','application/json');
     request.send(JSON.stringify({username:username}));
 };
